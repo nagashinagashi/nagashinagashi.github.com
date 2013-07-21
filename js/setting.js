ef7910a62431239412d29294b841b7b45eb240ad
nagashinagashi <nagashima.econ@gmail.com>
2013-07-21 22:09:19 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -18,16 +18,16 @@ OpenSpending.Styles = OpenSpending.Styles || {};
 OpenSpending.Styles.Cofog = {
 
   /* fukuroi cofog */
-  '1': { icon: 'icons/communication.svg', color: '#C75746', bcolor: '#935B3B' },
-  '2': { icon: 'icons/economic-aid.svg', color: '#C75746', bcolor: '#0AB971' },
+  '1': { icon: 'icons/social-system.svg', color: '#C75746', bcolor: '#935B3B' },
+  '2': { icon: 'icons/waste.svg', color: '#C75746', bcolor: '#0AB971' },
   '3': { icon: 'icons/forest.svg', color: '#C75746', bcolor: '#4E6D00' },
   '4': { icon: 'icons/health.svg', color: '#C75746', bcolor: '#D33673' },
-  '5': { icon: 'icons/schools.svg', color: '#C75746', bcolor: '#2A3A03' },
-  '6': { icon: 'icons/goverment.svg', color: '#C75746', bcolor: '#EC2406' },
+  '5': { icon: 'icons/pre-school.svg', color: '#C75746', bcolor: '#2A3A03' },
+  '6': { icon: 'icons/goverment-uk.svg', color: '#C75746', bcolor: '#EC2406' },
   '7': { icon: 'icons/fire-brigade.svg', color: '#C75746', bcolor: '#790586' },
   '8': { icon: 'icons/tree.svg', color: '#C75746', bcolor: '#C75746' },
-  '9': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#D33673' },
+  '9': { icon: 'icons/pig.svg', color: '#C75746', bcolor: '#D33673' },
   '10': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#790586' },
   '11': { icon: 'icons/labour.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '12': { icon: 'icons/polution.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '12': { icon: 'icons/hospital.svg', color: '#C75746', bcolor: '#4E6D00' },
 };
